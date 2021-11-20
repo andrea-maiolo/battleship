6fa4913f371c4af7a99ecba4d1ae1cdae55c462a
andrea-maiolo <maiolo.m.2@gmail.com>
2021-11-20 20:01:35 +0100

    working on the user interface

--- src/index.js
+++ src/index.js
@@ -3,29 +3,48 @@ import shipFactory from "./factories/shipFactory";
 import playerFactory from "./factories/playerFactory";
 import cp from "./computerPlayer/cp";
 
+//this is initialization
 const skynet = cp();
 const andy = new playerFactory("andy");
 const andyBoard = new gameBoardFactory();
 
 const andyPatrol = new shipFactory('patrol');
-const andyCurser = new shipFactory('curser');
-const andySub = new shipFactory('sub');
-const andyBattleship = new shipFactory('battleship');
-const andyAdmiral = new shipFactory('admiral');
+// const andyCurser = new shipFactory('curser');
+// const andySub = new shipFactory('sub');
+// const andyBattleship = new shipFactory('battleship');
+// const andyAdmiral = new shipFactory('admiral');
 
-andyBoard.shipAllocation(andyPatrol, 3, "x");
+andyBoard.shipAllocation(andyPatrol, 3, "y");
 
+
+//this is dom
 let body = document.querySelector('body');
-let playerContainer = document.createElement('div');
-playerContainer.id= "playerContainer";
-let cpContainer = document.createElement('div');
-cpContainer.id = "cpContainer";
-body.appendChild(playerContainer);
-body.appendChild(cpContainer);
+let containerPlayer = document.querySelector('#containerPlayer');
+let containerComputer = document.querySelector('#containerComputer');
 
+//display player 's grid
 for (let i = 0; i < andyBoard.gameBoard.length; i++) {
-    playerContainer.appendChild(document.createElement('div'))
+    let cell = andyBoard.gameBoard[i];
+    let cellDiv = document.createElement('div');
+    if(cell.shipObj){cellDiv.classList.add("ship")}
+    cellDiv.id = cell.id;
+    cellDiv.classList.add('playerGridCell')
+    containerPlayer.appendChild(cellDiv);
 }
-// for (let i = 0; i < 256; i++) {
-//     container.appendChild(document.createElement('div'))
-// }
+
+//making the grid a grid(if it makes sense)
+containerPlayer.style.gridTemplateColumns = "repeat(10, 1fr)";
+containerPlayer.style.gridTemplateRows = "repeat(10, 1fr)";
+containerComputer.style.gridTemplateColumns = "repeat(10, 1fr)";
+containerComputer.style.gridTemplateRows = "repeat(10, 1fr)";
+
+//display computer's grid
+for (let i = 0; i < skynet.cpBoard.gameBoard.length; i++) {
+    let cell = skynet.cpBoard.gameBoard[i];
+    let cellDiv = document.createElement('div');
+    cellDiv.id = cell.id;
+    cellDiv.classList.add('computerGridCell')
+    containerComputer.appendChild(cellDiv);
+}
+
+
